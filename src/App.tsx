382dfe0ae3b6d176654d03531c31048e378bfab0
Th0mes <contato.thomestg@gmail.com>
2022-06-20 19:42:21 -0300

    feat: :sparkles: Main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,62 @@
-import { useState } from "react";
-import logo from "./logo.svg";
-import poweredBy from "./powered-by-vitawind-dark.png";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Circles } from "react-loader-spinner";
+import { motion, AnimatePresence } from "framer-motion";
 
-function App() {
-  const [count, setCount] = useState(0);
+const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+
+export default function App() {
+  const [data, setData] = useState({ data: [] });
+  const [isLoading, setIsLoading] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleClick = async () => {
+    setIsLoading(true);
+    try {
+      const { data } = await axios.get("https://reqres.in/api/users", {
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+      setData(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+      setIsVisible(true);
+    }
+  };
+
+  console.log(data);
+
+  const number = 123456789;
+
+  useEffect
 
   return (
-    <div className="text-center selection:bg-green-900">
-      <header className="flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-white">
-        <img
-          src={logo}
-          className="animate-speed h-60 motion-safe:animate-spin"
-          alt="logo"
-        />
-        <style>
-          {
-            "\
-            .animate-speed{\
-              animation-duration:20s;\
-            }\
-          "
-          }
-        </style>
-        <p className="bg-gradient-to-r from-emerald-300 to-sky-300 bg-clip-text text-5xl font-black text-transparent selection:bg-transparent">
-          Vite + React + Typescript + Tailwindcss v3
-        </p>
-        <p className="mt-3">
-          <button
-            type="button"
-            className="my-6 rounded bg-gray-300 px-2 py-2 text-[#282C34] transition-all hover:bg-gray-200"
-            onClick={() => setCount((count) => count + 1)}
+    <div className="flex h-screen flex-col items-center justify-center bg-slate-200 dark:bg-slate-900">
+      <button
+        onClick={handleClick}
+        className="absolute rounded-xl bg-gradient-to-l from-[#007991] to-[#78ffd6] px-6 py-4 text-xl text-white transition-all duration-300 hover:contrast-125 focus:scale-125 dark:from-[#C33764] dark:to-[#1D2671]"
+      >
+        Who Asked?{" "}
+        {/* {isLoading && <Circles color="#00BFFF" height={24} width={24} />} */}
+      </button>
+
+      <AnimatePresence>
+        {isVisible && (
+          <motion.h2
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="relative top-20 text-4xl text-white"
           >
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code className="text-[#8d96a7]">App.tsx</code> and save to test
-          HMR updates.
-        </p>
-        <p className="mt-3 flex gap-3 text-center text-[#8d96a7]">
-          <a
-            className="text-[#61dafb] transition-all hover:text-blue-400"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="text-[#61dafb] transition-all hover:text-blue-400"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-        <img src={poweredBy} className="mx-auto my-8" alt="powered-by" />
-      </header>
+            A total of {new Intl.NumberFormat().format(number)} people don't
+            give a fuck!
+          </motion.h2>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
-
-export default App;
